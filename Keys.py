7d08c32fdf542dbda5c3673a0124612c9bfd0c69
Kavithvajen <kavithvajen@gmail.com>
2019-11-23 14:12:20 +0000

    Going public.

---
+++ Keys.py
@@ -0,0 +1,15 @@
+def getFitbitClientID():
+    Client_ID = "XXXXX"
+    return Client_ID
+
+def getFitbitClientSecret():
+    Client_Secret = "XXXXXXXX"
+    return Client_Secret
+
+def getEmailID():
+    Email_ID = "XXXXXX@gmail.com"
+    return Email_ID
+
+def getEmailPassword():
+    Password = "XXXXXX"
+    return Password
